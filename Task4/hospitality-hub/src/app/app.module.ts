9c04b1e3972edbfd687203ff91d3e10fb51a1c5d
Arsenii Merkulov <arsenii.vm@gmail.com>
2024-05-20 16:30:18 +0300

    initial angular setup

---
+++ Task4/hospitality-hub/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+//import {ApiModule} from "./api-proxy/api.module";
+import {environment} from "../environments/environment";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from "./shared/shared.module";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    //ApiModule.forRoot({rootUrl: environment.apiUrl}),
+    BrowserAnimationsModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ Task4/hospitality-hub/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:25601/api',
+  locales: ['en', 'ua'],
+  defaultLocale: 'en',
+};
